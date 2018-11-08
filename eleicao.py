a3c6e3fc232757fe71ac6a51d910257e90a7ff03
Michael Santos <michael-santos@outlook.com.br>
2018-11-08 17:23:05 -0200

    Adicionado nomes dos intergrates

---
+++ eleicao.py
@@ -0,0 +1,7 @@
+##############################################
+# Eleição de líder em ambientes sem fio
+# Intergrantes: 
+#   Michael dos Santos
+#   Washington Paes Marques da Silva
+##############################################
+
